783d7c37c56c7a209302c72a7f6f25057529796f
Thomas ten Cate <ttencate@gmail.com>
2012-08-19 15:29:39 +0100

    Implement framebuffer and part of texture

--- src/gladder.js
+++ src/gladder.js
@@ -1,14 +1,19 @@
 /**
  * args can contain:
  * canvas: canvas DOM node, or id of one (required)
- * debug: set to true to log errors to console (requires webgl-debug.js)
+ * debug: set to true to log WebGL errors to console (requires webgl-debug.js)
  * errorCallback: called with error string on error if debug is true
+ * callCallback: called with call signature string on every WebGL call if debug is true
  */
 function Gladder(args) {
 
   var canvas;
   var gl;
   var gla = this;
+
+  ///////////////
+  // UTILITIES //
+  ///////////////
 
   function getElement(element) {
     if (element === null) {
@@ -25,24 +30,82 @@ function Gladder(args) {
     return element;
   };
 
+  function setDefaultArgs(args, defaultArgs) {
+    for (var key in defaultArgs) {
+      if (!defaultArgs.hasOwnProperty(key)) continue;
+      if (!args.hasOwnProperty(key) || args[key] === undefined) {
+        args[key] = defaultArgs[key];
+      }
+    }
+  };
+
+  /////////////
+  // CONTEXT //
+  /////////////
+
+  setDefaultArgs(args, {
+    debug: false,
+    errorCallback: null,
+    callCallback: null,
+  });
+
+  function getGl(canvas) {
+    var CONTEXT_NAMES = ["webgl", "experimental-webgl", "moz-webgl", "webkit-3d"];
+    for (var i = 0; i < CONTEXT_NAMES.length; ++i) {
+      var gl = canvas.getContext(CONTEXT_NAMES[i]);
+      if (gl !== null) {
+        break;
+      }
+    }
+    if (gl === null) {
+      throw new Error("WebGL not supported");
+    }
+    return gl;
+  }
+
+  canvas = getElement(args.canvas);
+  gl = getGl(canvas);
+
+  if (args.debug) {
+    if (WebGLDebugUtils === undefined) {
+      throw new Error("To use debug mode, you need to load webgl-debug.js. Get it from http://www.khronos.org/webgl/wiki/Debugging");
+    }
+    var onError = null;
+    if (args.errorCallback !== null) {
+      onError = function(err, funcName, passedArguments) {
+        args.errorCallback(
+            WebGLDebugUtils.glEnumToString(err) + " in call " +
+            funcName + "(" + Array.prototype.slice.call(passedArguments).join(", ") + ")");
+      };
+    }
+    var onCall = null;
+    if (args.callCallback !== null) {
+      onCall = function(funcName, passedArguments) {
+        args.callCallback(
+            funcName + "(" + Array.prototype.slice.call(passedArguments).join(", ") + ")");
+      };
+    }
+    gl = WebGLDebugUtils.makeDebugContext(gl, onError, onCall);
+  }
+
+  this.canvas = canvas;
+
   ////////////
   // BASICS //
   ////////////
 
-  this.setClearColor = function(red, green, blue, alpha) {
-    gl.clearColor(red, green, blue, alpha === undefined ? 1.0 : alpha);
-  };
-
-  this.setClearDepth = function(depth) {
-    gl.clearDepth(depth);
-  };
-
   this.clear = function(args) {
+    // TODO cache current color/depth
+    if (args.color) {
+      gl.clearColor(args.color[0], args.color[1], args.color[2], args.color[3] !== undefined ? args.color[3] : 1.0);
+    }
+    if (args.depth) {
+      gl.clearDepth(args.depth);
+    }
     gl.clear(
-        args.colorBuffer ? gl.COLOR_BUFFER_BIT : 0 |
-        args.depthBuffer ? gl.DEPTH_BUFFER_BIT : 0 |
-        args.stencilBuffer ? gl.STENCIL_BUFFER_BIT : 0
-        );
+        args.color ? gl.COLOR_BUFFER_BIT : 0 |
+        args.depth ? gl.DEPTH_BUFFER_BIT : 0 |
+        args.stencil ? gl.STENCIL_BUFFER_BIT : 0);
   };
 
   ////////////////
@@ -50,21 +113,24 @@ function Gladder(args) {
   ////////////////
   
   function createStateBitFunc(funcName, bitName) {
-    gla[funcName] = function(enable) {
+    gla["enable" + funcName] = function(enable) {
       if (enable || enable === undefined) {
         gl.enable(gl[bitName]);
       } else {
         gl.disable(gl[bitName]);
       }
     };
+    gla["disable" + funcName] = function() {
+      gl.disable(gl[bitName]);
+    };
   }
 
-  createStateBitFunc('enableBlend', 'BLEND');
-  createStateBitFunc('enableCullFace', 'CULL_FACE');
-  createStateBitFunc('enableDepthTest', 'DEPTH_TEST');
-  createStateBitFunc('enableDither', 'DITHER');
-  createStateBitFunc('enablePolygonOffsetFill', 'POLYGON_OFFSET_FILL');
-  createStateBitFunc('enableStencilTest', 'STENCIL_TEST');
+  createStateBitFunc('Blend', 'BLEND');
+  createStateBitFunc('CullFace', 'CULL_FACE');
+  createStateBitFunc('DepthTest', 'DEPTH_TEST');
+  createStateBitFunc('Dither', 'DITHER');
+  createStateBitFunc('PolygonOffsetFill', 'POLYGON_OFFSET_FILL');
+  createStateBitFunc('StencilTest', 'STENCIL_TEST');
 
   ///////////////
   // ANIMATION //
@@ -121,14 +187,17 @@ function Gladder(args) {
   /////////////
   
   this.Buffer = function(args) {
-    if (args.type === undefined) args.type = Float32Array;
-    if (args.normalized === undefined) args.normalized = false;
-    if (args.stride === undefined) args.stride = 0;
+    setDefaultArgs(args, {
+      type: Float32Array,
+      normalized: false,
+      stride: 0,
+      usage: this.constructor.Usage.STATIC_DRAW,
+    });
 
     var glBuffer = gl.createBuffer();
 
     gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new args.type(args.data), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new args.type(args.data), args.usage);
 
     this.componentsPerItem = args.componentsPerItem;
     this.numItems = args.data.length / args.componentsPerItem;
@@ -160,6 +229,12 @@ function Gladder(args) {
     };
   };
 
+  this.Buffer.Usage = {
+    DYNAMIC_DRAW: gl.DYNAMIC_DRAW,
+    STATIC_DRAW: gl.STATIC_DRAW,
+    STREAM_DRAW: gl.STREAM_DRAW,
+  };
+
   /////////////
   // SHADERS //
   /////////////
@@ -178,22 +253,15 @@ function Gladder(args) {
   };
 
   this.Shader = function(args) {
-    var source = null;
+    var source;
     try {
       source = getScriptContents(args.source);
     } catch (e) {
       // Assume we were passed raw shader source code
-      source = shader;
+      source = args.source;
     }
 
-    var type = null;
-    if (args.vertexShader) {
-      type = gl.VERTEX_SHADER;
-    } else if (args.fragmentShader) {
-      type = gl.FRAGMENT_SHADER;
-    } else {
-      throw new Error("Either vertexShader or fragmentShader must be set to true upon Shader construction");
-    }
+    var type = args.type;
 
     var glShader = gl.createShader(type);
     compileShader(glShader, source);
@@ -206,6 +274,11 @@ function Gladder(args) {
     };
   }
 
+  this.Shader.Type = {
+    VERTEX_SHADER: gl.VERTEX_SHADER,
+    FRAGMENT_SHADER: gl.FRAGMENT_SHADER,
+  };
+
   //////////////
   // UNIFORMS //
   //////////////
@@ -214,9 +287,11 @@ function Gladder(args) {
     var value = null;
 
     var suffix = {
-      "float": "1f", "vec2": "2f", "vec3": "3f", "vec4": "4f",
+      "bool": "1i",
       "int": "1i", "ivec2": "2i", "ivec3": "3i", "ivec4": "4i",
+      "float": "1f", "vec2": "2f", "vec3": "3f", "vec4": "4f",
       "mat2": "2f", "mat3": "3f", "mat4": "4f",
+      "sampler2D": "1i", "samplerCube": "1i",
     }[type];
     if (suffix === undefined) {
       throw new Error("Unknown uniform type " + type);
@@ -242,6 +317,9 @@ function Gladder(args) {
       if (args[args.length - 1][0] !== undefined) {
         // Received an array
         vSetter.apply(gl, args);
+      } else if (args[1] instanceof TextureUnit) {
+        // Received a texture unit
+        setter.call(gl, args[1]._id);
       } else if (setter !== null) {
         // Received some scalars
         setter.apply(gl, args);
@@ -329,8 +407,8 @@ function Gladder(args) {
   this.Program = function(args) {
     var glProgram = gl.createProgram();
     // TODO accept Shader objects for vertexShader and fragmentShader args
-    var vertexShader = new gla.Shader({ source: args.vertexShader, vertexShader: true });
-    var fragmentShader = new gla.Shader({ source: args.fragmentShader, fragmentShader: true });
+    var vertexShader = new gla.Shader({ source: args.vertexShader, type: gla.Shader.Type.VERTEX_SHADER });
+    var fragmentShader = new gla.Shader({ source: args.fragmentShader, type: gla.Shader.Type.FRAGMENT_SHADER });
     linkProgram(glProgram, vertexShader, fragmentShader);
 
     this.uniforms = {};
@@ -341,7 +419,6 @@ function Gladder(args) {
         var glUniform = gl.getUniformLocation(glProgram, name);
         if (glUniform === null) {
           console.log("Warning: uniform " + name + " not found in program");
-          continue;
         }
         this.uniforms[name] = new Uniform(glUniform, type);
       }
@@ -355,7 +432,6 @@ function Gladder(args) {
         var glAttribute = gl.getAttribLocation(glProgram, name);
         if (glAttribute === null) {
           console.log("Warning: attribute " + name + " not found in program");
-          continue;
         }
         this.attributes[name] = new Attribute(glAttribute, type);
       }
@@ -366,9 +442,144 @@ function Gladder(args) {
     }
   };
 
+  ///////////////////
+  // TEXTURE UNITS //
+  ///////////////////
+
+  function TextureUnit(index) {
+    var glUnit = gl.TEXTURE0 + index;
+    this._id = index;
+    this.withActivated = function(func) {
+      if (index !== this.constructor.active) {
+        gl.activeTexture(glUnit);
+        this.constructor.active = index;
+      }
+      func();
+    };
+  }
+
+  TextureUnit.active = 0;
+
+  function createTextureUnits() {
+    var textureUnits = [];
+    var count = gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS);
+    for (var i = 0; i < count; ++i) {
+      textureUnits[i] = new TextureUnit(i);
+    }
+    return textureUnits;
+  }
+
+  this.textureUnits = createTextureUnits();
+
   //////////////
   // TEXTURES //
   //////////////
+
+  this.Texture = function(args) {
+    setDefaultArgs(args, {
+      target: this.constructor.Target.TEXTURE_2D,
+      minFilter: this.constructor.Filter.NEAREST_MIPMAP_LINEAR,
+      magFilter: this.constructor.Filter.LINEAR,
+      wrapS: this.constructor.Wrap.REPEAT,
+      wrapT: this.constructor.Wrap.REPEAT,
+    });
+
+    var glTexture = gl.createTexture();
+    this._id = glTexture;
+
+    var target = args.target;
+
+    this.withBound = function(unit, func) {
+      if (func === undefined) {
+        func = unit;
+        unit = 0;
+      }
+      gla.textureUnits[unit].withActivated(function() {
+        gl.bindTexture(target, glTexture);
+        func();
+      });
+    };
+
+    this.setFilter = function(min, mag) {
+      this.withBound(function() {
+        gl.texParameteri(target, gl.TEXTURE_MIN_FILTER, min);
+        gl.texParameteri(target, gl.TEXTURE_MAG_FILTER, mag);
+      });
+    };
+
+    this.setWrap = function(s, t) {
+      this.withBound(function() {
+        gl.texParameteri(target, gl.TEXTURE_WRAP_S, s);
+        gl.texParameteri(target, gl.TEXTURE_WRAP_T, t);
+      });
+    };
+
+    this.setImage = function(args) {
+      setDefaultArgs(args, {
+        target: this.constructor.Target.TEXTURE_2D,
+        level: 0,
+        format: this.constructor.Format.RGBA,
+        type: this.constructor.Type.UNSIGNED_BYTE,
+        pixels: null,
+      });
+      this.withBound(function() {
+        gl.texImage2D(args.target, args.level, args.format, args.width, args.height, 0, args.format, args.type, args.pixels);
+      });
+    };
+
+    this.setFilter(args.minFilter, args.magFilter);
+    this.setWrap(args.wrapS, args.wrapT);
+    if (args.width !== undefined && args.height !== undefined) {
+      this.setImage({
+        width: args.width,
+        height: args.height,
+        format: args.format,
+        type: args.type,
+        pixels: args.pixels,
+      });
+    }
+  };
+
+  this.Texture.Target = {
+    TEXTURE_2D: gl.TEXTURE_2D,
+    CUBE_MAP: gl.CUBE_MAP,
+    CUBE_MAP_POSITIVE_X: gl.TEXTURE_CUBE_MAP_POSITIVE_X,
+    CUBE_MAP_NEGATIVE_X: gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+    CUBE_MAP_POSITIVE_Y: gl.TEYTURE_CUBE_MAP_POSITIVE_Y,
+    CUBE_MAP_NEGATIVE_Y: gl.TEYTURE_CUBE_MAP_NEGATIVE_Y,
+    CUBE_MAP_POSITIVE_Z: gl.TEZTURE_CUBE_MAP_POSITIVE_Z,
+    CUBE_MAP_NEGATIVE_Z: gl.TEZTURE_CUBE_MAP_NEGATIVE_Z,
+  };
+  this.Texture.Filter = {
+    NEAREST: gl.NEAREST,
+    LINEAR: gl.LINEAR,
+    NEAREST_MIPMAP_NEAREST: gl.NEAREST_MIPMAP_NEAREST,
+    LINEAR_MIPMAP_NEAREST: gl.LINEAR_MIPMAP_NEAREST,
+    NEAREST_MIPMAP_LINEAR: gl.NEAREST_MIPMAP_LINEAR,
+    LINEAR_MIPMAP_LINEAR: gl.LINEAR_MIPMAP_LINEAR,
+  };
+  this.Texture.Wrap = {
+    CLAMP_TO_EDGE: gl.CLAMP_TO_EDGE,
+    MIRRORED_REPEAT: gl.MIRRORED_REPEAT,
+    REPEAT: gl.REPEAT,
+  };
+  this.Texture.Format = {
+    ALPHA: gl.ALPHA,
+    LUMINANCE: gl.LUMINANCE,
+    LUMINANCE_ALPHA: gl.LUMINANCE_ALPHA,
+    RGB: gl.RGB,
+    RGBA: gl.RGBA,
+  };
+  this.Texture.Type = {
+    UNSIGNED_BYTE: gl.UNSIGNED_BYTE,
+    UNSIGNED_SHORT_5_6_5: gl.UNSIGNED_SHORT_5_6_5,
+    UNSIGNED_SHORT_4_4_4_4: gl.UNSIGNED_SHORT_4_4_4_4,
+    UNSIGNED_SHORT_5_5_5_1: gl.UNSIGNED_SHORT_5_5_5_1,
+  };
+
+  ///////////////////
+  // RENDERBUFFERS //
+  ///////////////////
 
   // TODO
 
@@ -376,50 +587,61 @@ function Gladder(args) {
   // FRAMEBUFFERS //
   //////////////////
 
-  // TODO
+  this.Framebuffer = function(args) {
+    var glFramebuffer = gl.createFramebuffer();
 
-  /////////////
-  // CONTEXT //
-  /////////////
+    this.withBound = function(func) {
+      gl.bindFramebuffer(gl.FRAMEBUFFER, glFramebuffer);
+      func();
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null); // TODO restore properly
+    };
 
-  function getGl(canvas) {
-    var CONTEXT_NAMES = ["webgl", "experimental-webgl", "moz-webgl", "webkit-3d"];
-    for (var i = 0; i < CONTEXT_NAMES.length; ++i) {
-      var gl = canvas.getContext(CONTEXT_NAMES[i]);
-      if (gl !== null) {
-        break;
-      }
-    }
-    if (gl === null) {
-      throw new Error("WebGL not supported");
-    }
-    return gl;
-  }
+    function attachBuffer(attachment, buffer) {
+      this.withBound(function() {
+        if (buffer instanceof gla.Texture) {
+          gl.framebufferTexture2D(gl.FRAMEBUFFER, attachment, gl.TEXTURE_2D, buffer._id, 0);
+        } else if (buffer instanceof gla.RenderBuffer) {
+          gl.framebufferRenderbuffer(gl.FRAMEBUFFER, attachment, gl.RENDERBUFFER, buffer._id);
+        }
+      });
+    };
 
-  canvas = getElement(args.canvas);
-  gl = getGl(canvas);
+    this.attachColorBuffer = function(colorBuffer) {
+      attachBuffer.call(this, gl.COLOR_ATTACHMENT0, colorBuffer);
+    };
 
-  if (args.debug) {
-    if (WebGLDebugUtils === undefined) {
-      throw new Error("To use debug mode, you need to load webgl-debug.js. Get it from http://www.khronos.org/webgl/wiki/Debugging");
-    }
-    var onError = null;
-    if (args.errorCallback) {
-      onError = function(err, funcName, passedArguments) {
-        args.errorCallback(
-            WebGLDebugUtils.glEnumToString(err) + " in call " +
-            funcName + "(" + Array.prototype.slice.call(passedArguments).join(", ") + ")");
-      };
-    }
-    var onCall = null;
-    if (args.callCallback) {
-      onCall = function(funcName, passedArguments) {
-        args.callCallback(
-            funcName + "(" + Array.prototype.slice.call(passedArguments).join(", ") + ")");
-      };
-    }
-    gl = WebGLDebugUtils.makeDebugContext(gl, onError, onCall);
-  }
+    this.attachDepthBuffer = function(depthBuffer) {
+      attachBuffer.call(this, gl.DEPTH_ATTACHMENT, depthBuffer);
+    };
 
-  this.canvas = canvas;
+    this.attachStencilBuffer = function(stencilBuffer) {
+      attachBuffer.call(this, gl.STENCIL_ATTACHMENT, stencilBuffer);
+    };
+
+    this.checkComplete = function() {
+      this.withBound(function() {
+        var framebufferStatus = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+        if (framebufferStatus !== gl.FRAMEBUFFER_COMPLETE) {
+          var textStatus = framebufferStatus;
+          switch (framebufferStatus) {
+            case gl.FRAMEBUFFER_INCOMPLETE_ATTACHMENT: textStatus = "INCOMPLETE_ATTACHMENT"; break;
+            case gl.FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT: textStatus = "INCOMPLETE_MISSING_ATTACHMENT"; break;
+            case gl.FRAMEBUFFER_INCOMPLETE_DIMENSIONS: textStatus = "INCOMPLETE_DIMENSIONS"; break;
+            case gl.FRAMEBUFFER_UNSUPPORTED: textStatus = "UNSUPPORTED"; break;
+          };
+          throw new Error("Framebuffer incomplete: " + textStatus);
+        }
+      });
+    };
+
+    if (args.colorBuffer) {
+      this.attachColorBuffer(args.colorBuffer);
+    }
+    if (args.depthBuffer) {
+      this.attachDepthBuffer(args.depthBuffer);
+    }
+    if (args.stencilBuffer) {
+      this.attachStencilBuffer(args.stencilBuffer);
+    }
+  };
 }
